87df602b6b909b5d5af28da909734618d98d0231
SigmaDigmma <33125817+SigmaDigmma@users.noreply.github.com>
2021-08-26 19:27:08 +0200

    arrow, spell

--- GitDungeonGame/Assets/Scripts/Arrow.cs
+++ GitDungeonGame/Assets/Scripts/Arrow.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Arrow : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
---
+++ GitDungeonGame/Assets/Scripts/Shooting.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public GameObject spellPrefab;
+    public int type; //1 Nahkampf 2 Bogen 3 Spell
+
+    public float arrowForce = 5f;
+    public float spellForce = 10f;
+    public float charge = 0f;
+
+    private void Update()
+    {
+        if (type == 2)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                arrowForce = arrowForce + charge / 20;
+                Shoot();
+            }
+        }
+        if(type == 3)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                ShootSpell();
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (type == 2)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                if (charge < 240)
+                {
+                    charge = charge + 1;
+                }
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * arrowForce, ForceMode2D.Impulse);
+        charge = 0f;
+        arrowForce = 5f;
+    }
+    void ShootSpell()
+    {
+        GameObject spell = Instantiate(spellPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = spell.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * spellForce, ForceMode2D.Impulse);
+    }
+}
