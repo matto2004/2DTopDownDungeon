545244aab8af6815cebb8ba22bb7cba0bb0bef32
matto2004 <77439788+matto2004@users.noreply.github.com>
2021-08-26 17:59:18 +0200

    Organisation

---
+++ GitDungeonGame/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float followSpeed = 10f;
+    void Update()
+    {
+        transform.position = target.position + offset;   
+    }
+}
