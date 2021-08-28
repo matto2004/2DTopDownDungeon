3122c80bb383e3e2b574727688e1b330535feddd
matto2004 <77439788+matto2004@users.noreply.github.com>
2021-08-28 22:48:28 +0200

    Idee

---
+++ GitDungeonGame/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    public GameObject DungeonButton;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DungeonButton.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            DungeonButton.SetActive(false);
+        }
+    }
+}
