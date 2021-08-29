7a6aa06b6b2a8048833b0905e093a012bcdefdce
SigmaDigmma <33125817+SigmaDigmma@users.noreply.github.com>
2021-08-29 17:31:06 +0200

    Update ProjectileArrow.cs

--- GitDungeonGame/Assets/Scripts/ProjectileArrow.cs
+++ GitDungeonGame/Assets/Scripts/ProjectileArrow.cs
@@ -4,35 +4,18 @@ using UnityEngine;
 
 public class ProjectileArrow : MonoBehaviour
 {
+    GameObject enemy;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Room"))
+        if (collision.CompareTag("Room"))
         {
-            Damage();
             Destroy(gameObject);
         }
-        else if (collision.gameObject.CompareTag("Enemy"))
+        else if (collision.CompareTag("Enemy"))
         {
-            Damage();
+            collision.GetComponent<Enemy>().takeDamage(this.gameObject.GetComponent<ProjectileStats>().Damage);
             Destroy(gameObject);
-        }
-    }
-    private void Damage()
-    {
-        var hitColliders = Physics2D.OverlapPointAll(transform.position);
-        foreach (var hitCollider in hitColliders)
-        {
-            var enemy = hitCollider.GetComponent<Enemy>();
-            if (enemy)
-            {
-                var closestPoint = hitCollider.ClosestPoint(transform.position);
-                var distance = Vector3.Distance(closestPoint, transform.position);
-
-                var damagePercent = 1;
-                enemy.takeDamage(damagePercent * this.gameObject.GetComponent<ProjectileStats>().Damage);
-                Debug.Log(this.gameObject.GetComponent<ProjectileStats>().Damage);
-            }
         }
     }
 
